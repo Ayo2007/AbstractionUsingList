c95c9e51a07c6239ed7bceac4f52e65cc93c47f6
Ayo <aogunkinle@lcs.on.ca>
2024-01-29 11:15:11 -0500

    Initial Commit

---
+++ AbstractionUsingList/AbstractionUsingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbstractionUsingListApp.swift
+//  AbstractionUsingList
+//
+//  Created by Ayodeji Ogunkinle on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct AbstractionUsingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
